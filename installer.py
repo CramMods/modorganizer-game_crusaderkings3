520d38273988bf8109ff6667c1604b6f9b315217
Cram42 <cram42@users.noreply.github.com>
2023-07-19 12:53:45 +0800

    Make TreeHelper static, pep8

--- installer.py
+++ installer.py
@@ -1,8 +1,8 @@
 try:
-    from PyQt6.QtCore import qCritical, qInfo
-except:
-    from PyQt5.QtCore import qCritical, qInfo
-    
+    from PyQt6.QtCore import qCritical, qInfo  # noqa:F401
+except Exception:
+    from PyQt5.QtCore import qCritical, qInfo  # noqa:F401
+
 import mobase
 
 from typing import List, Union
@@ -12,14 +12,11 @@ from .localization import localize_string
 
 
 class ArchiveInstaller(mobase.IPluginInstallerSimple):
-
     _descriptor: Descriptor
     _organizer: mobase.IOrganizer
-    _tree_helper: TreeHelper
 
     def __init__(self):
         super().__init__()
-        self._tree_helper = TreeHelper()
 
     # IPlugin Implementation
 
@@ -29,35 +26,39 @@ class ArchiveInstaller(mobase.IPluginInstallerSimple):
 
     def name(self) -> str:
         return "Crusader Kings III Archive Installer"
-    
+
     def localizedName(self) -> str:
         return localize_string(self.name())
-    
+
     def author(self) -> str:
         return "Cram42"
 
     def description(self) -> str:
-        return localize_string("Adds support for installing Crusader Kings III mod archives")
-    
+        return localize_string(
+            "Adds support for installing Crusader Kings III mod archives"
+        )
+
     def version(self) -> mobase.VersionInfo:
         return mobase.VersionInfo(0, 1, 0)
-    
+
     def isActive(self) -> bool:
         return self._organizer.pluginSetting(self.name(), "enabled")
-    
+
     def settings(self) -> List[mobase.PluginSetting]:
         return [
-            mobase.PluginSetting("enabled", localize_string("Check to enable this plugin"), True)
+            mobase.PluginSetting(
+                "enabled", localize_string("Check to enable this plugin"), True
+            )
         ]
 
     # IPluginInstaller Implementation
 
     def isArchiveSupported(self, tree: mobase.IFileTree) -> bool:
-        return self._tree_helper.can_be_installable(tree)
+        return TreeHelper.can_be_installable(tree)
 
     def isManualInstaller(self) -> bool:
         return False
-    
+
     def priority(self) -> int:
         return 50
 
@@ -67,8 +68,12 @@ class ArchiveInstaller(mobase.IPluginInstallerSimple):
             try:
                 version = mobase.VersionInfo(self._descriptor.version())
                 new_mod.setVersion(version)
-            except:
-                qInfo("[{}] failed to set version to {}".format(new_mod.name(), version))
+            except Exception:
+                qInfo(
+                    "[{}] failed to set version to {}".format(
+                        new_mod.name(), version
+                    )
+                )
                 pass
 
             # Set categories
@@ -77,10 +82,14 @@ class ArchiveInstaller(mobase.IPluginInstallerSimple):
             for category in self._descriptor.categories():
                 try:
                     new_mod.addCategory(category)
-                except:
-                    qInfo("[{}] failed to add category {}".format(new_mod.name(), category))
+                except Exception:
+                    qInfo(
+                        "[{}] failed to add category {}".format(
+                            new_mod.name(), category
+                        )
+                    )
                     pass
-        
+
     # IPluginInstallerSimple Implementation
 
     def install(
@@ -88,30 +97,31 @@ class ArchiveInstaller(mobase.IPluginInstallerSimple):
         name: mobase.GuessedString,
         tree: mobase.IFileTree,
         version: str,
-        modId: int
+        modId: int,
     ) -> Union[mobase.InstallResult, mobase.IFileTree]:
-    
-        if not self._tree_helper.can_be_installable(tree):
+        if not TreeHelper.can_be_installable(tree):
             qCritical("Install Failed: can_be_installable = False")
             return mobase.InstallResult.NOT_ATTEMPTED
 
-        install_tree = self._tree_helper.to_installable(tree)
+        install_tree = TreeHelper.to_installable(tree)
         if not install_tree:
             qCritical("Install Failed: to_installable returned None")
             return mobase.InstallResult.FAILED
 
-        descriptor_entry = self._tree_helper.find_descriptor(install_tree)
+        descriptor_entry = TreeHelper.find_descriptor(install_tree)
         descriptor_path = self._manager().extractFile(descriptor_entry)
         self._descriptor = Descriptor(descriptor_path)
 
         new_name = self._descriptor.name()
         safe_chars = [" ", ".", "_"]
-        new_name = "".join(c for c in new_name if c.isalnum() or c in safe_chars).rstrip()
+        new_name = "".join(
+            c for c in new_name if c.isalnum() or c in safe_chars
+        ).rstrip()
         name.update(new_name)
 
-        final_tree = self._tree_helper.to_final(install_tree)
+        final_tree = TreeHelper.to_final(install_tree)
         if not final_tree:
             qCritical("Install Failed: to_final returned None")
             return mobase.InstallResult.FAILED
-        
+
         return final_tree
--- mod.py
+++ mod.py
@@ -1,22 +1,20 @@
 try:
-    from PyQt6.QtCore import qInfo
-except:
-    from PyQt5.QtCore import qInfo
-    
+    from PyQt6.QtCore import qInfo  # noqa:F401
+except Exception:
+    from PyQt5.QtCore import qInfo  # noqa:F401
+
 import mobase
 import re
 
 from typing import List
 
 
-class Descriptor():
-
+class Descriptor:
     _name: str
     _version: str
     _categories: List[str]
 
     def __init__(self, descriptor_path: str):
-
         self._name = ""
         self._version = ""
         self._categories = []
@@ -31,12 +29,14 @@ class Descriptor():
         version_match = re.search(r"^version=\"(.+)\"$", content, re.MULTILINE)
         if version_match:
             self._version = version_match.group(1)
-        
+
         tags_match = re.search(r"tags={\n*(.*)\n*}", content, re.DOTALL)
         if tags_match:
             tags_chunk = tags_match.group(1)
-            self._categories = re.findall(r"^\s*\"(.+)\"\s*$", tags_chunk, re.MULTILINE)
-    
+            self._categories = re.findall(
+                r"^\s*\"(.+)\"\s*$", tags_chunk, re.MULTILINE
+            )
+
     def name(self) -> str:
         return self._name
 
@@ -45,97 +45,119 @@ class Descriptor():
 
     def categories(self) -> List[str]:
         return self._categories
-        
 
 
-class TreeHelper():
-
-    _valid_dirs: List[str]
-    _valid_files: List[str]
-
-    def __init__(self):
-        super().__init__()
-        self._valid_dirs = [
-            "common", "content_source",
-            "data_binding", "dlc", "dlc_metadata",
-            "events",
-            "fonts",
-            "gfx", "gui",
-            "history",
-            "licenses", "localization",
-            "map_data", "music",
-            "notifications",
-            "sound",
-            "tests", "tools", "tweakergui_assets"
-        ]
-        self._valid_files = []
+class TreeHelper:
+    VALID_DIRS: List[str] = [
+        "common",
+        "content_source",
+        "data_binding",
+        "dlc",
+        "dlc_metadata",
+        "events",
+        "fonts",
+        "gfx",
+        "gui",
+        "history",
+        "licenses",
+        "localization",
+        "map_data",
+        "music",
+        "notifications",
+        "sound",
+        "tests",
+        "tools",
+        "tweakergui_assets",
+    ]
 
     # Descriptors & Content
 
-    def find_descriptor(self, tree: mobase.IFileTree, deep: bool = False) -> mobase.FileTreeEntry:
-        descriptor = self.find_mod_descriptor(tree, deep=deep)
+    @staticmethod
+    def find_descriptor(
+        tree: mobase.IFileTree, deep: bool = False
+    ) -> mobase.FileTreeEntry:
+        descriptor = TreeHelper.find_mod_descriptor(tree, deep=deep)
         if not descriptor:
-            descriptor = self.find_install_descriptor(tree, deep=deep)
+            descriptor = TreeHelper.find_install_descriptor(tree, deep=deep)
         return descriptor
 
-    def find_mod_descriptor(self, tree: mobase.IFileTree, deep: bool = False) -> mobase.FileTreeEntry:
-        descriptor = tree.find("descriptor.mod", mobase.FileTreeEntry.FileTypes.FILE)
+    @staticmethod
+    def find_mod_descriptor(
+        tree: mobase.IFileTree, deep: bool = False
+    ) -> mobase.FileTreeEntry:
+        descriptor = tree.find(
+            "descriptor.mod", mobase.FileTreeEntry.FileTypes.FILE
+        )
         if descriptor:
             return descriptor
-        
+
         if deep:
             for entry in tree:
                 if entry.isDir():
-                    descriptor = self.find_mod_descriptor(entry, True)
+                    descriptor = TreeHelper.find_mod_descriptor(entry, True)
                     if descriptor:
                         return descriptor
 
         return None
 
-    def find_install_descriptor(self, tree: mobase.IFileTree, deep: bool = False) -> mobase.FileTreeEntry:
+    @staticmethod
+    def find_install_descriptor(
+        tree: mobase.IFileTree, deep: bool = False
+    ) -> mobase.FileTreeEntry:
         for entry in tree:
             if entry.isFile():
                 if entry.suffix() == "mod":
                     return entry
-        
+
         if deep:
             for entry in tree:
                 if entry.isDir():
-                    descriptor = self.find_install_descriptor(entry, True)
+                    descriptor = TreeHelper.find_install_descriptor(
+                        entry, True
+                    )
                     if descriptor:
                         return descriptor
 
         return None
 
-    def get_content_source(self, descriptor: mobase.FileTreeEntry) -> mobase.IFileTree:
+    @staticmethod
+    def get_content_source(
+        descriptor: mobase.FileTreeEntry,
+    ) -> mobase.IFileTree:
         if descriptor.name().casefold() == "descriptor.mod":
             return descriptor.parent()
-        
+
         source_name = descriptor.name()[:-4]
-        source_dir = descriptor.parent().find(source_name, mobase.FileTreeEntry.FileTypes.DIRECTORY)
+        source_dir = descriptor.parent().find(
+            source_name, mobase.FileTreeEntry.FileTypes.DIRECTORY
+        )
         return source_dir
 
     # Validate Directories
 
-    def has_dirs(self, tree: mobase.IFileTree) -> bool:
+    @staticmethod
+    def has_dirs(tree: mobase.IFileTree) -> bool:
         for entry in tree:
             if entry.isDir():
                 return True
         return False
 
-    def validate_dir(self, odir: mobase.FileTreeEntry) -> bool:
-        return odir.name().casefold() in self._valid_dirs
+    @staticmethod
+    def validate_dir(odir: mobase.FileTreeEntry) -> bool:
+        return odir.name().casefold() in TreeHelper.VALID_DIRS
 
-    def validate_dirs(self, tree: mobase.IFileTree) -> bool:
+    @staticmethod
+    def validate_dirs(tree: mobase.IFileTree) -> bool:
         for entry in tree:
             if entry.isDir():
-                if not self.validate_dir(entry):
+                if not TreeHelper.validate_dir(entry):
                     return False
         return True
 
     # Validate Files
 
-    def has_files(self, tree: mobase.IFileTree) -> bool:
+    @staticmethod
+    def has_files(tree: mobase.IFileTree) -> bool:
         for entry in tree:
             if entry.isFile():
                 return True
@@ -143,77 +165,83 @@ class TreeHelper():
 
     # Installable Layout
 
-    def can_be_installable(self, tree: mobase.IFileTree) -> bool:
-        descriptor = self.find_descriptor(tree, deep=True)
+    @staticmethod
+    def can_be_installable(tree: mobase.IFileTree) -> bool:
+        descriptor = TreeHelper.find_descriptor(tree, deep=True)
         if descriptor:
-            content = self.get_content_source(descriptor)
+            content = TreeHelper.get_content_source(descriptor)
             if content:
-                return self.is_installable(content, ignore_descriptor=True)
+                return TreeHelper.is_installable(
+                    content, ignore_descriptor=True
+                )
         return False
 
-    def is_installable(self, tree: mobase.IFileTree, ignore_descriptor: bool = True) -> bool:
+    @staticmethod
+    def is_installable(
+        tree: mobase.IFileTree, ignore_descriptor: bool = True
+    ) -> bool:
         # Installable layout contains a descriptor and valid folders
         # We don't care about files
         if not ignore_descriptor:
-            descriptor = self.find_mod_descriptor(tree, deep=False)
+            descriptor = TreeHelper.find_mod_descriptor(tree, deep=False)
             if not descriptor:
                 return False
-        if not self.has_dirs(tree):
+        if not TreeHelper.has_dirs(tree):
             return False
-        return self.validate_dirs(tree)
-    
-    def to_installable(self, tree: mobase.IFileTree) -> mobase.IFileTree:
+        return TreeHelper.validate_dirs(tree)
+
+    @staticmethod
+    def to_installable(tree: mobase.IFileTree) -> mobase.IFileTree:
         new_tree = tree.createOrphanTree("")
-        descriptor = self.find_descriptor(tree, deep=True)
+        descriptor = TreeHelper.find_descriptor(tree, deep=True)
         if descriptor:
             new_tree.copy(descriptor, "descriptor.mod")
-            content = self.get_content_source(descriptor)
+            content = TreeHelper.get_content_source(descriptor)
             for entry in content:
                 if entry.isDir():
                     new_tree.copy(entry)
-            return new_tree if self.is_installable(new_tree) else None
+            return new_tree if TreeHelper.is_installable(new_tree) else None
         return None
 
     # Final Layout
 
-    def is_final(self, tree: mobase.IFileTree) -> bool:
+    @staticmethod
+    def is_final(tree: mobase.IFileTree) -> bool:
         # Final layout contains only valid folders
-        if self.has_files(tree):
+        if TreeHelper.has_files(tree):
             return False
-        if not self.has_dirs(tree):
+        if not TreeHelper.has_dirs(tree):
             return False
-        return self.validate_dirs(tree)
-    
-    def to_final(self, tree: mobase.IFileTree) -> mobase.IFileTree:
-        tree = self.to_installable(tree)
+        return TreeHelper.validate_dirs(tree)
+
+    @staticmethod
+    def to_final(tree: mobase.IFileTree) -> mobase.IFileTree:
+        tree = TreeHelper.to_installable(tree)
         new_tree = tree.createOrphanTree("")
         for entry in tree:
             if entry.isDir():
                 new_tree.copy(entry)
-        return new_tree if self.is_final(new_tree) else None
+        return new_tree if TreeHelper.is_final(new_tree) else None
 
 
 class ModDataChecker(mobase.ModDataChecker):
-
-    _tree_helper: TreeHelper
-
     def __init__(self):
         super().__init__()
-        self._tree_helper = TreeHelper()
 
-    def dataLooksValid(self, tree: mobase.IFileTree) -> mobase.ModDataChecker.CheckReturn:
-
+    def dataLooksValid(
+        self, tree: mobase.IFileTree
+    ) -> mobase.ModDataChecker.CheckReturn:
         # Final mod layout, all good
-        if self._tree_helper.is_final(tree):
+        if TreeHelper.is_final(tree):
             return mobase.ModDataChecker.CheckReturn.VALID
 
         # Installer mod layout, still has descriptor.mod
-        if self._tree_helper.is_installable(tree):
+        if TreeHelper.is_installable(tree):
             return mobase.ModDataChecker.CheckReturn.FIXABLE
 
         # Invalid mod layout
         return mobase.ModDataChecker.CheckReturn.INVALID
-    
+
     def fix(self, tree: mobase.IFileTree) -> mobase.IFileTree:
         # Fix takes us from Installer -> Final
-        return self._tree_helper.to_final(tree)
+        return TreeHelper.to_final(tree)
